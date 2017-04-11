7b2a886bdb760098bd5d5dd77fc719b4712328be
azizzakiryarov <azizzakiryarov@Azizs-MacBook-Pro.local>
2017-04-11 13:06:32 +0200

    Klart

--- src/test/java/se/groupfish/RestCaseManagementApplicationTests.java
+++ src/test/java/se/groupfish/RestCaseManagementApplicationTests.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import se.groupfish.testrestcasemanagement.testresourse.IssueResourseMock;
 import se.groupfish.testrestcasemanagement.testresourse.TeamResourseMock;
 import se.groupfish.testrestcasemanagement.testresourse.UserResourseMock;
 import se.groupfish.testrestcasemanagement.testresourse.WorkItemResourseMock;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UserResourseMock.class, TeamResourseMock.class, WorkItemResourseMock.class })
+@SuiteClasses({ UserResourseMock.class, TeamResourseMock.class, WorkItemResourseMock.class, IssueResourseMock.class })
 public class RestCaseManagementApplicationTests {
 
 }
